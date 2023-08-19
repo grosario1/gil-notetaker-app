331431dd0215614ec21513c6425625cc32409c09
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-19 17:52:09 -0400

    updated app listen line

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -57,6 +57,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+// For local testing uncomment the next line and comment that does not contain localhost url
+// app.listen(PORT, () =>console.log(`App listening at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
