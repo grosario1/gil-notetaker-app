1045a1b8447376d55364ed14eefd806f5585e385
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-19 14:31:24 -0400

    update code in server.js

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
 // GET Route for home page
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
@@ -23,32 +21,42 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET route for wildcard pages
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 // GET Route for api notes
 app.get('/api/notes', (req, res) => {
-    const notesData = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
-    const notes = JSON.parse(notesData);
-    res.json(notes);
-  });  
+  const notesData = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+  const notes = JSON.parse(notesData);
+  res.json(notes);
+});
 
-//POST to db.json
+// POST Route for saving a new note
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    const notesData = fs.readFileSync('./db/db.json', 'utf8');
-    const notes = JSON.parse(notesData);
-  
-    newNote.id = Date.now().toString();
-  
-    notes.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-    res.json(newNote);
-  });
-  
-  app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
-  );
+  const newNote = req.body;
+  const notesData = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+  const notes = JSON.parse(notesData);
 
+  newNote.id = Date.now().toString();
+
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(notes));
+  res.json(newNote);
+});
+
+// DELETE Route for deleting a note
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  let notesData = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+  const notes = JSON.parse(notesData);
+
+  const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+  fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(updatedNotes));
+  res.json({ message: 'Note deleted successfully' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
